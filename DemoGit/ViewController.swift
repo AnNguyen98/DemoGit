cc0bb2a54c45a0e4aff9f8556802b3be1a04b377
AnNguyen98 <theannguyen98@gmail.com>
2018-08-20 00:51:20 +0700

    mesenger

--- DemoGit/ViewController.swift
+++ DemoGit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Change made in new branch")
     }
 
     override func didReceiveMemoryWarning() {
